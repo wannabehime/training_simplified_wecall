e162803c21abf6ce1ee65d17aebf596a0c572103
Hajime <aqr.kai.4@gmail.com>
2025-01-31 17:26:16 +0900

    Revert "fix: drop reservation slots table"
    This reverts commit eea8d84e83284cff28026eb36e4b32ca5f6f3a13.

--- backend/prisma/migrations/20250130035652_change_gender_type_to_string/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `gender` on the `Entries` table. The data in that column could be lost. The data in that column will be cast from `Enum(EnumId(0))` to `VarChar(191)`.
-
-*/
--- AlterTable
-ALTER TABLE `Entries` MODIFY `gender` VARCHAR(191) NOT NULL;
