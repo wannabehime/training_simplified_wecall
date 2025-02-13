2c8086ccdffffadabb732315daa6949622dfbcb8
Hajime <aqr.kai.4@gmail.com>
2025-02-13 19:11:44 +0900

    fix/delete_repeated_code

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,13 +7,6 @@ import App from './App.vue'
 import router from './router'
 
 const liffId = import.meta.env.VITE_LIFF_ID as string
-await liff.init({ liffId: '2006858338-7J5NbWdO', withLoginOnExternalBrowser: true }).then(() => {
-  if (window.location.pathname === '/') {
-    window.location.href = '/entry'
-  }
-  const idToken = liff.getIDToken();
-  console.log(idToken);
-})
 
 const app = createApp(App)
 app.use(createPinia())
