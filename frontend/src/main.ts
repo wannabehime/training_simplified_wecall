2832a487df0d8a64c9ac37cf6588f06f5db4595b
Hajime <aqr.kai.4@gmail.com>
2025-02-13 16:14:28 +0900

    fix/use-vue-router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,15 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-await liff.init({ liffId: '2006858338-7J5NbWdO', withLoginOnExternalBrowser: true }).then(() => {
-  if (window.location.pathname === '/') {
-    window.location.href = '/entry'
-  }
-})
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
+
+await liff.init({ liffId: liffId, withLoginOnExternalBrowser: true }).then(() => {
+  if (router.currentRoute.value.path === '/') {
+    router.push('/entry')
+  }
+})
 
 app.mount('#app')
