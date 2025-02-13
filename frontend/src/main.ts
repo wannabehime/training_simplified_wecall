3199f5721aac5847c78fb024e846d6fdd3777e27
Hajime <aqr.kai.4@gmail.com>
2025-02-13 16:16:29 +0900

    fix/make_liffId_env_variable

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+const liffId = import.meta.env.VITE_LIFF_ID as string
 
 const app = createApp(App)
 app.use(createPinia())
