7e9b8d68c33dbabbae470b6e1e6ea3427332b74b
Hajime <aqr.kai.4@gmail.com>
2025-02-16 19:46:19 +0900

    fix/init_liff_in_App.vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,21 +1,12 @@
 import './assets/main.css'
-import liff from '@line/liff'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const liffId = import.meta.env.VITE_LIFF_ID as string
-
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-
-await liff.init({ liffId: liffId, withLoginOnExternalBrowser: true }).then(() => {
-  if (router.currentRoute.value.path === '/') {
-    router.push('/entry')
-  }
-})
 
 app.mount('#app')
