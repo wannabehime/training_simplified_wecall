a4a8761bab88979870aec3056ac89ab2dd9843ea
Hajime <aqr.kai.4@gmail.com>
2025-02-28 11:06:14 +0900

    add/test_file

---
+++ backend/src/test.ts
@@ -0,0 +1,20 @@
+// import { convertEntryToChangeMessage } from './line/convertEntryToMessage';
+
+// console.log(
+//   convertEntryToChangeMessage({
+//     familyName: 'a',
+//     personalName: 'a',
+//     familyNameKana: 'a',
+//     personalNameKana: 'a',
+//     gender: '男性',
+//     birthday: '',
+//     prefecture: '',
+//     tel: '',
+//     email: '',
+//     isAccompanied: true,
+//     visitDay: '2015-01-01T00:00:00.000Z',
+//     visitTime: '12:00',
+//   }),
+// );
+
+
